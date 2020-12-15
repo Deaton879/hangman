882457c0ec9f5a6a5b8c265e709d30bc677abbbb
Nathaniel SNow <64047255+NathanielSnow@users.noreply.github.com>
2020-12-14 20:26:44 -0600

    Update Words.java
    Added documentation

--- src/Hangman/Words.java
+++ src/Hangman/Words.java
@@ -2,25 +2,57 @@ package Hangman;
 
 import java.util.ArrayList;
 
+/**
+ * The Words Class
+ * Purpose: This is a derived class. It extends the Strings class.
+ * @author Dallas Eaton
+ * @author Nathaniel SNow
+ */
 public class Words extends Strings {
 
     protected String category;
 
+    /**
+     * The Words class constructor
+     * Purpose: Constructs a default Words object.
+     * @param category
+     * @param name
+     */
     public Words(String category, String name) {
         super(name); this.setCategory(category);
     }
 
+    /**
+     * The setCategory method
+     * Purpose: Sets category.
+     * @param category
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * The setContent method
+     * Purpose: Sets content. Overrides Strings class setContent method
+     * @param content
+     */
     @Override
     public void setContent(String content) {
         super.setContent(content);
     }
 
+    /**
+     * The getCategory method
+     * Purpose: Returns category.
+     * @return string
+     */
     public String getCategory() { return this.category; }
 
+    /**
+     * The getContent method
+     * Purpose: Returns content. Overrides Strings class getContent method
+     * @return string
+     */
     @Override
     public String getContent() { return super.getContent(); }
 
