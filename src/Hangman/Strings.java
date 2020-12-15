9015ab74f0c1176b2a5f9f97687a72af47f465d4
Nathaniel SNow <64047255+NathanielSnow@users.noreply.github.com>
2020-12-14 20:26:12 -0600

    Update Strings.java
    Added documentation

--- src/Hangman/Strings.java
+++ src/Hangman/Strings.java
@@ -1,19 +1,29 @@
 package Hangman;
 
+/**
+ * The Strings Class
+ * Purpose: This is a base class.
+ * @author Dallas Eaton
+ * @author Nathaniel SNow
+ */
+
 public class Strings {
 
     protected String content;
 
-
+    /** Default values of the Strings class*/
     public Strings() {
     }
 
+    /** Constructs a default Strings object */
     public Strings(String content) {
         this.setContent(content);
     }
 
+    /** Return content */
     public String getContent() { return this.content; }
 
+    /** Set new content */
     public void setContent(String content) { this.content = content; }
 
 }
